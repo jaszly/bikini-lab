d8af705a961f0dd7e9064c3972d432c82ae59d8e
jaszly <jasvmurray@gmail.com>
2022-03-04 13:29:42 -0800

    updated place example

--- src/places.js
+++ src/places.js
@@ -13,17 +13,17 @@ const PlacesList = () => {
   const [places, setPlaces] = useState([
     {
       id: 1,
-      cityName: "Atlanta",
-      type: "at night",
-      eat: "Poor Calvins",
-      mustTryFood: "Lamb Ossoburro",
-      drink: "Poor Calvins",
-      mustTryDrink: "vodka martini",
-      see: "midtown",
-      especiallySee: "piedmont park ",
-      mustDo: "music in piedmont park",
-      explore: "midtown",
-      budget: "200",
+      cityName: "Paris",
+      type: "in a morning",
+      eat: "Boulange",
+      mustTryFood: "pan du fromage",
+      drink: "cafe de atlier",
+      mustTryDrink: "parisian latte",
+      see: "le minoff",
+      especiallySee: "park",
+      mustDo: "selfie from the ne corner of tour effiel",
+      explore: "le marais",
+      budget: "20",
     },
   ]);
 
@@ -69,16 +69,30 @@ const PlacesList = () => {
       <>
         <div style={{ textAlign: "center", textTransform: "lowercase" }}>
           <Row>
-            <h3 style={{ fontSize: "10vh", textTransform: "capitalize" }}>
-              {cityName}
-            </h3>
-            <p className="type">{type}</p>
-            <p className="user">by @user</p>
+            <div>
+              <h3
+                style={{
+                  fontFamily: "Raleway",
+                  fontSize: "10vh",
+                  textTransform: "capitalize",
+                }}
+              >
+                <i className="fas fa-map-marker-alt"></i>
+                {cityName}
+              </h3>
+            </div>
+            <p className="type">
+              <i class="far fa-clock"></i>
+              {type}
+            </p>
+            <p style={{ fontSize: "15px" }} className="user">
+              by @user
+            </p>
           </Row>
 
           <img
-            style={{ width: " 40vw", margin: "4vh 0 -4vh 0" }}
-            src="https://images.unsplash.com/photo-1589414480645-9c552d67f352?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YXRsYW50YXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
+            style={{ width: " 40vw", margin: "4vh 0 -4vh 0", opacity: "60%" }}
+            src="https://images.unsplash.com/photo-1500301111609-42f1aa6df72a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
             class="img-thumbnail"
             alt="..."
           />
@@ -98,7 +112,7 @@ const PlacesList = () => {
                 src={
                   "https://images.unsplash.com/photo-1532636875304-0c89119d9b4d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
                 }
-                class="img-thumbnail"
+                className="img-thumbnail"
                 alt="..."
               />
             </div>
@@ -113,7 +127,7 @@ const PlacesList = () => {
               <Row> Must try: {mustTryDrink}</Row>
             </Col>
             <div>
-              <img src="..." class="img-thumbnail" alt="..." />
+              <img src="..." className="img-thumbnail" alt="..." />
             </div>
           </Row>
 
@@ -126,7 +140,7 @@ const PlacesList = () => {
               <Row> especially: {especiallySee}</Row>
             </Col>
             <div>
-              <img src="..." class="img-thumbnail" alt="..." />
+              <img src="..." className="img-thumbnail" alt="..." />
             </div>
           </Row>
 
@@ -146,17 +160,17 @@ const PlacesList = () => {
             </div>
             <Row>{explore}</Row>
             <div>
-              <img src="..." class="img-thumbnail" alt="..." />
+              <img src="..." className="img-thumbnail" alt="..." />
             </div>
           </Row>
-          <hr />
 
           <Row className="grid two  ">
             <div>
-              <p>budget</p>
+              <h3 style={{ marginLeft: "2vw" }}>budget:</h3>
             </div>
-            <Row>{budget}</Row>
+            <Row>${budget}</Row>
           </Row>
+          <hr />
         </div>
       </>
     );

da8d2f8f0ed19a800f44fd9acbb79eee7ac92af0
jaszly <jasvmurray@gmail.com>
2022-03-21 09:52:20 -0700

    remove content add city img

--- src/Lab.js
+++ src/Lab.js
@@ -19,7 +19,7 @@ const WanderLab = ({ index, onCreatePlace }) => {
   const [mustDo, setmustDo] = useState("");
   const [explore, setexplore] = useState("");
   const [budget, setbudget] = useState(0);
-  const [content, setContent] = useState("");
+  const [cityImg, setcityImg] = useState("");
 
   const createPlace = (event) => {
     event.preventDefault();
@@ -37,7 +37,7 @@ const WanderLab = ({ index, onCreatePlace }) => {
       mustDo,
       explore,
       budget,
-      content,
+      cityImg,
     };
     onCreatePlace(place);
 
@@ -54,7 +54,7 @@ const WanderLab = ({ index, onCreatePlace }) => {
     setmustDo("");
     setexplore("");
     setbudget("");
-    setContent("");
+    setcityImg("");
   };
   const Place = ({
     cityName,
@@ -69,7 +69,7 @@ const WanderLab = ({ index, onCreatePlace }) => {
     mustDo,
     explore,
     budget,
-    content,
+    cityImg,
     onShowPlace,
     onCalculateTotal,
   }) => {
@@ -96,146 +96,176 @@ const WanderLab = ({ index, onCreatePlace }) => {
             <hr />
           </div>
 
-          <Form>
-            <Form.Label style={{ border: "none", marginTop: "2vh" }}>
-              your user name:
-            </Form.Label>
-            <Form.Control
-              type="text"
-              value={userName}
-              placeholder="enter a username"
-              onChange={(e) => setuserName(e.target.value)}
-            />
-            <Form.Group className="mb-1">
-              <Form.Label style={{ border: "none", marginTop: "2vh" }}>
-                City + Time of day:
-              </Form.Label>
+          <div className="text-editor">
+            <Form style={{ border: ".5px solid #7b8a96" }}>
               <Form.Control
+                style={{
+                  marginBottom: "2vh",
+                  marginLeft: "1vw",
+                  padding: "2vw",
+                }}
                 type="text"
-                value={cityName}
-                placeholder="name of city"
-                onChange={(e) => setcityName(e.target.value)}
+                value={userName}
+                placeholder="@ create username"
+                onChange={(e) => setuserName(e.target.value)}
               />
+              <hr className="underline" style={{ marginBottom: "4vh" }} />
+              <span className="form-section" style={{ color: "grey" }}>
+                CITY:
+              </span>
 
+              <Form.Group>
+                <Form.Control
+                  style={{ marginTop: "-3vh" }}
+                  className="aboveline"
+                  type="text"
+                  value={cityName}
+                  placeholder="name of city:"
+                  onChange={(e) => setcityName(e.target.value)}
+                />
+                <hr className="underline" />
+
+                <Form.Control
+                  type="file"
+                  name="file"
+                  value={cityImg}
+                  className="form-control bubbles"
+                  onChange={(e) => setcityImg(e.target.value)}
+                />
+
+                <Form.Select
+                  className="form-control bubbles"
+                  aria-label="Default select example"
+                  id="type"
+                  name="type"
+                  value={type}
+                  onChange={(e) => settype(e.target.value)}
+                >
+                  <option selected>(Select time frame)</option>
+                  <option value="in a morning">
+                    in a morning (12pm and earlier)
+                  </option>
+                  <option value="in an afternoon">
+                    in an afternoon (6 hours or less)
+                  </option>
+                  <option value="in an evening">
+                    in an evening (after 5pm)
+                  </option>
+                  <option value="at night">at night (late night) </option>
+                </Form.Select>
+
+                <div style={{ border: ".5px solid #384a5a" }}>
+                  <span
+                    className="form-section"
+                    style={{ color: "grey", margin: "3vw" }}
+                  >
+                    FOOD + DRINK
+                  </span>
+                  <Form.Label></Form.Label>
+                  <Form.Control
+                    style={{ marginBottom: "-3vw" }}
+                    className="aboveline"
+                    type="text"
+                    value={eat}
+                    placeholder="restaurant or cafe"
+                    onChange={(e) => seteat(e.target.value)}
+                  />
+                  <br /> <br />
+                  <hr className="underline" />
+                  <Form.Group
+                    style={{
+                      margin: "2vh 1vw 0 1vw",
+                      border: "0.5px solid rgb(123, 138, 150)",
+                      borderRadius: "30px",
+                      padding: "5vh",
+                    }}
+                  >
+                    <Form.Control
+                      type="text"
+                      value={mustTryFood}
+                      placeholder="best thing to order:"
+                      onChange={(e) => setmustTryFood(e.target.value)}
+                      style={{ margin: "-23px -2vw" }}
+                    />
+
+                    <Form.Control
+                      type="text"
+                      value={drink}
+                      placeholder="grab a beverage here:"
+                      onChange={(e) => setdrink(e.target.value)}
+                      style={{ margin: "54px -2vw" }}
+                    />
+
+                    <Form.Control
+                      type="text"
+                      value={mustTryDrink}
+                      placeholder="best drink(s):"
+                      onChange={(e) => setmustTryDrink(e.target.value)}
+                      style={{ margin: "54px -2vw" }}
+                    />
+                    <hr />
+                  </Form.Group>
+                </div>
+              </Form.Group>
+            </Form>
+            <Form>
+              <Form.Group className="mb-1">
+                <br />
+                <br />
+              </Form.Group>
               <br />
+              <Form.Group className="mb-1">
+                <Form.Label>
+                  Must activity landmark/area/historic site etc:
+                </Form.Label>
+                <Form.Control
+                  className="input-lrg"
+                  type="text"
+                  value={activity}
+                  onChange={(e) => setActivity(e.target.value)}
+                />
+                <Form.Label> Especially: </Form.Label>
+                <Form.Control
+                  type="textarea"
+                  value={especiallySee}
+                  onChange={(e) => setespeciallySee(e.target.value)}
+                />
+                <Form.Label>
+                  {" "}
+                  Must do activity/excursion/tour/experience:{" "}
+                </Form.Label>
+                <Form.Control
+                  className="input-lrg"
+                  type="text"
+                  value={mustDo}
+                  onChange={(e) => setmustDo(e.target.value)}
+                />
+                <Form.Label> Best thing to explore {type}: </Form.Label>
+                <Form.Control
+                  className="input-lrg"
+                  type="text"
+                  value={explore}
+                  onChange={(e) => setexplore(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Label> Budget </Form.Label>
               <Form.Select
                 className="form-control"
-                aria-label="Default select example"
-                id="type"
-                name="type"
-                value={type}
-                onChange={(e) => settype(e.target.value)}
+                aria-label="  select budget"
+                id="budget"
+                name="budget"
+                value={budget}
+                onChange={(e) => setbudget(e.target.value)}
               >
-                {" "}
-                <option selected>Select time frame</option>
-                <option value="in a morning">
-                  in a morning (12pm and earlier)
-                </option>
-                <option value="in an afternoon">
-                  in an afternoon (6 hours or less)
-                </option>
-                <option value="in an evening">in an evening (after 5pm)</option>
-                <option value="at night">at night (late night) </option>
+                <option selected>Select budget</option>
+                <option value="$">low cost</option>
+                <option value="$$">medium cost</option>
+                <option value="$$$">high cost</option>
               </Form.Select>
-              <br />
-              <Form.Label> Upload a photo </Form.Label>
-
-              <Form.Control type="file" name="file" className="form-control" />
-            </Form.Group>
-            <br />
-            <Form.Group className="mb-1">
-              <Form.Label> best place to eat {type}: </Form.Label>
-              <Form.Control
-                type="text"
-                value={eat}
-                placeholder="best place to eat"
-                onChange={(e) => seteat(e.target.value)}
-              />
-              <br />
-              <Form.Control
-                type="text"
-                value={mustTryFood}
-                placeholder="must try food"
-                onChange={(e) => setmustTryFood(e.target.value)}
-              />{" "}
-              <br />
-              <Form.Label> Grab a beverage {type} </Form.Label>
-              <Form.Control
-                type="text"
-                value={drink}
-                placeholder="grab a beverage here"
-                onChange={(e) => setdrink(e.target.value)}
-              />{" "}
-              <br />
-              <Form.Control
-                type="text"
-                value={mustTryDrink}
-                placeholder="your top reccomended beverage(s)"
-                onChange={(e) => setmustTryDrink(e.target.value)}
-              />
-            </Form.Group>
-            <br />
-            <Form.Group className="mb-1">
-              <Form.Label>
-                {" "}
-                Must activity landmark/area/historic site etc:{" "}
-              </Form.Label>
-              <Form.Control
-                className="input-lrg"
-                type="text"
-                value={activity}
-                onChange={(e) => setActivity(e.target.value)}
-              />
-              <Form.Label> Especially: </Form.Label>
-              <Form.Control
-                type="textarea"
-                value={especiallySee}
-                onChange={(e) => setespeciallySee(e.target.value)}
-              />
-              <Form.Label>
-                {" "}
-                Must do activity/excursion/tour/experience:{" "}
-              </Form.Label>
-              <Form.Control
-                className="input-lrg"
-                type="text"
-                value={mustDo}
-                onChange={(e) => setmustDo(e.target.value)}
-              />
-              <Form.Label> Best thing to explore {type}: </Form.Label>
-              <Form.Control
-                className="input-lrg"
-                type="text"
-                value={explore}
-                onChange={(e) => setexplore(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Label> Budget </Form.Label>
-            <Form.Select
-              className="form-control"
-              aria-label="  select budget"
-              id="budget"
-              name="budget"
-              value={budget}
-              onChange={(e) => setbudget(e.target.value)}
-            >
-              <option selected>Select budget</option>
-              <option value="$">$ low cost</option>
-              <option value="$$">$$ moderate cost</option>
-              <option value="$$$">$$$ medium cost</option>
-              <option value="$$$$">$$$$ high cost</option>
-            </Form.Select>
-            <br />
-            <Form.Label> Content </Form.Label>
-            <Form.Control
-              type="text"
-              value={content}
-              onChange={(e) => setContent(e.target.value)}
-            />{" "}
-            <br />
-            <Button onClick={createPlace}> Add to Wander Lab</Button>
-            <hr />
-          </Form>
+              <Button onClick={createPlace}> Add to Wander Lab</Button>
+              <hr />
+            </Form>
+          </div>
         </Col>
       </Row>
     </Container>
--- src/places.js
+++ src/places.js
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./style.css";
 import WanderLab from "./Lab.js";
 
-import { Button, Container, Col, Row } from "react-bootstrap";
+import { Button, Card, Container, Col, Row } from "react-bootstrap";
 
 //create place obj
 
@@ -16,6 +16,8 @@ const PlacesList = () => {
       userName: "jaszly",
       cityName: "San Francisco",
       type: "in an afternoon",
+      cityImg:
+        "https://images.unsplash.com/photo-1425321395722-b1dd54a97cf3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
       eat: "Hog Island Oyster co",
       mustTryFood:
         "any of the local oysters to start. mussels, the shrimp po boy and fries for the table.",
@@ -55,7 +57,7 @@ const PlacesList = () => {
     mustDo,
     explore,
     budget,
-    content,
+    cityImg,
     onShowPlace,
   }) => {
     //Declare a new state var called quantity
@@ -71,119 +73,105 @@ const PlacesList = () => {
         <div style={{ textAlign: "center", textTransform: "lowercase" }}>
           <Row style={{ textAlign: "center" }}>
             <div>
-              <h3
-                style={{
-                  fontFamily: "Noto Serif Display",
-                  fontSize: "10vh",
-                  textTransform: "lowercase",
-                }}
-              >
+              <h3 className="city">
                 <i className="fas fa-map-marker-alt"></i>
                 {cityName}
               </h3>
             </div>
-            <p className="type">{type}</p>
+            <p className="time-of-day">{type}</p>
             <p style={{ fontSize: "15px" }} className="user">
               by @{userName}
             </p>
           </Row>
           <hr />
 
-          <img
-            style={{ width: " 40vw", margin: "4vh 0 -4vh 0", opacity: "80%" }}
-            src="https://images.unsplash.com/photo-1425321395722-b1dd54a97cf3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-            className="img-thumbnail"
-            alt="..."
-          />
-          <Row className="grid three sections">
-            <div>
-              <h3 className="subtitle">eat</h3>
-            </div>
-            <Col>
-              <Row className="input-primary">"{eat}"</Row>
-              <Row>
-                <p
-                  className="subtitle-secondary"
-                  style={{ margin: "1vh -4vw}" }}
-                >
-                  Must try:
-                </p>
-                <p className="description">"{mustTryFood}"</p>
-              </Row>
-            </Col>
-            <div>
-              <img
-                className="img-thumbnail little-imgs"
-                src={
-                  "https://images.unsplash.com/photo-1627898292764-6733087b55ac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8b3lzdGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-                }
-                alt="..."
-              />
-            </div>
-          </Row>
-          <hr />
-          <Row className="grid three sections">
-            <div>
-              <h3 className="subtitle">drink</h3>
-              <Row>
-                <p className="subtitle-secondary">Must try: </p>
-              </Row>
-            </div>
-            <Col>
-              <Row style={{ marginTop: "2vh" }} className="input-primary">
-                "{drink}"
-              </Row>
+          <div
+            style={{ backgroundImage: `url('${cityImg}')` }}
+            className="city-img"
+          ></div>
 
-              <Row>
-                <p className="description" style={{ marginTop: "3vh" }}>
-                  "{mustTryDrink}"
-                </p>
-              </Row>
-            </Col>
-            <div>
-              <img
-                src="https://cdn.vox-cdn.com/thumbor/eER6G3tGnquawRm-ehn9QIdYr9E=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22436364/RedBayCoffee_PChang_3243.jpg"
-                className="img-thumbnail little-imgs"
-                alt="..."
-              />
-            </div>
-          </Row>
-          <hr />
+          <div class="gridplaces">
+            <div class="grid-item">
+              <Card className="cards">
+                <Card.Img
+                  variant="top"
+                  src="https://images.unsplash.com/photo-1621247473481-88661d45084e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+                />
+                <Card.Body>
+                  <p className="card-section">eat</p>
 
-          <Row className="grid three sections">
-            <div>
-              <h3 className="subtitle">do</h3>
+                  <Card.Title>
+                    <h1 className="card-subtitle">"{eat}"</h1>
+                  </Card.Title>
+                  <Card.Text className="card-description">
+                    "try: {mustTryFood}"
+                  </Card.Text>
+                </Card.Body>
+              </Card>{" "}
             </div>
+            <div class="grid-item">
+              {" "}
+              <Card className="cards">
+                <Card.Img
+                  variant="top"
+                  src="https://cdn.vox-cdn.com/thumbor/eER6G3tGnquawRm-ehn9QIdYr9E=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22436364/RedBayCoffee_PChang_3243.jpg"
+                />
+                <Card.Body>
+                  <p className="card-section">drink</p>
+                  <Card.Title>
+                    <h1 className="card-subtitle">"{drink}"</h1>
+                  </Card.Title>
+                  <Card.Text className="card-description">
+                    "order: {mustTryDrink}"
+                  </Card.Text>
+                </Card.Body>
+              </Card>{" "}
+            </div>
+            <div class="grid-item">
+              {" "}
+              <Card className="cards">
+                <Card.Img
+                  variant="top"
+                  src="https://images.unsplash.com/photo-1621247473481-88661d45084e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+                />
+                <Card.Body>
+                  <p className="card-section">do</p>
+
+                  <Card.Title>
+                    <h1 className="card-subtitle">"{activity}"</h1>
+                  </Card.Title>
+                  <Card.Text className="card-description">
+                    "try: {mustDo}"
+                  </Card.Text>
+                </Card.Body>
+              </Card>{" "}
+            </div>
+            <div class="grid-item">
+              {" "}
+              <Card className="cards">
+                <Card.Img
+                  variant="top"
+                  src="https://images.unsplash.com/photo-1621247473481-88661d45084e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+                />
+                <Card.Body>
+                  <p className="card-section">explore</p>
+
+                  <Card.Title>
+                    <h1 className="card-subtitle">"{explore}"</h1>
+                  </Card.Title>
+                  <Card.Text className="card-description">
+                    "especially: {especiallySee}"
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
+
+          <Row className="budget">
             <Col>
-              <Row className="input-primary">{activity}</Row>
-              <Row className="subtitle-secondary"></Row>
+              <h3 className="card-section">budget:</h3>
             </Col>
-            <Row style={{ fontWeight: "400" }} className="description">
-              "{mustDo}"
-            </Row>
-          </Row>
-          <hr />
-
-          <Row className="grid two sections">
-            <div>
-              <h3 className="subtitle">explore</h3>
-            </div>
-            <Col>
-              <Row className="input-primary">{explore}</Row>
-            </Col>
-            <div style={{ fontWeight: "400" }} className="description">
-              {especiallySee}
-            </div>
-          </Row>
-          <hr />
-
-          <Row className="grid two sections" style={{ margin: "0 11vw" }}>
-            <div>
-              <h3 className="subtitle">budget:</h3>
-            </div>
-            <Row className="input-primary" style={{ fontSize: "5vh" }}>
-              {budget}
-            </Row>
+            <Col className="price">{budget}</Col>
           </Row>
           <hr />
         </div>
@@ -214,7 +202,7 @@ const PlacesList = () => {
               mustDo={p.mustDo}
               explore={p.explore}
               budget={p.budget}
-              content={p.content}
+              cityImg={p.cityImg}
               onShowPlace={showPlace}
             />
           ))}
